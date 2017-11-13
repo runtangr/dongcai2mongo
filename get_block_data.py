293a47ff1eb53f3ab601f808b64960af31d92148
tangrui <1048393815@qq.com>
2017-11-13 09:14:11 +0800

    modify addr

--- get_block_data.py
+++ get_block_data.py
@@ -3,7 +3,7 @@ import datetime
 
 now_date = datetime.datetime.now()
 now_date_str = now_date.strftime("%Y-%m-%d")
-url = "http://10.9.4.29:8058/{0}.txt".format(now_date_str)
+url = "http://10.3.131.4:8012/{0}.txt".format(now_date_str)
 r = requests.get(url) 
 with open("dongcai_block_data.txt", "wb") as code:
      code.write(r.content)
